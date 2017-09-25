a054078a353f8f292fa3d1066fa712158e68f20b
Gaz1676 <gazfleming@gmail.com>
2017-09-25 15:59:30 +0100

    Added accounts.js to keep account details in

---
+++ app/controllers/accounts.js
@@ -0,0 +1,65 @@
+'use strict';
+
+exports.main = {
+
+  handler: function (request, reply) {
+    reply.view('main', { title: 'Welcome to Donations' });
+  },
+
+};
+
+exports.signup = {
+
+  handler: function (request, reply) {
+    reply.view('signup', { title: 'Sign up for Donations' });
+  },
+
+};
+
+exports.login = {
+
+  handler: function (request, reply) {
+    reply.view('login', { title: 'Login to Donations' });
+  },
+
+};
+
+exports.register = {
+
+  handler: function (request, reply) {
+    const user = request.payload;
+    this.users[user.email] = user;
+    reply.redirect('/login');
+  },
+
+};
+
+exports.authenticate = {
+
+  handler: function (request, reply) {
+    const user = request.payload;
+    if ((user.email in this.users) && (user.password === this.users[user.email].password)) {
+      this.currentUser = this.users[user.email];
+      reply.redirect('/home');
+    } else {
+      reply.redirect('/signup');
+    }
+  },
+
+};
+
+exports.logout = {
+
+  handler: function (request, reply) {
+    reply.redirect('/');
+  },
+
+};
+
+exports.about = {
+
+  handler: function (request, reply) {
+    reply.view('about', { title: 'About Donations' });
+  },
+
+};
