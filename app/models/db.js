667af14ac5c8163e8b78ca6bb3e0f890ffbfb1e8
Gaz1676 <gazfleming@gmail.com>
2017-10-02 19:43:32 +0100

    Created new db.js file which will establish a connection to the database

---
+++ app/models/db.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+// changed to localhost:12345
+let dbURI = 'mongodb://localhost:12345/donation';
+if (process.env.NODE_ENV === 'production') {
+  dbURI = process.env.MONGOLAB_URI;
+}
+
+mongoose.connect(dbURI);
+
+mongoose.connection.on('connected', function () {
+  console.log('Mongoose connected to ' + dbURI);
+});
+
+mongoose.connection.on('error', function (err) {
+  console.log('Mongoose connection error: ' + err);
+});
+
+mongoose.connection.on('disconnected', function () {
+  console.log('Mongoose disconnected');
+});
+
+// This will establish a connection to the database.
