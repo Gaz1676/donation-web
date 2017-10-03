4c34b0293f4e8324d36f3ce69a96d81def477c7c
Gaz1676 <gazfleming@gmail.com>
2017-10-03 12:09:47 +0100

    imported models/user and created a check on finding the user by email and displaying the data

--- app/models/donation.js
+++ app/models/donation.js
@@ -2,10 +2,22 @@
 
 const mongoose = require('mongoose');
 
-const donationSchema = mongoose.Schema({
+/*const donationSchema = mongoose.Schema({
   amount: Number,
   method: String,
   donor: String, // included to schema model to store some information about the donor
+});*/
+
+// retrieve further information on the donor with additional query.
+// using an object reference directly to the User object
+
+const donationSchema = mongoose.Schema({
+  amount: Number,
+  method: String,
+  donor: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  },
 });
 
 const Donation = mongoose.model('Donation', donationSchema);
