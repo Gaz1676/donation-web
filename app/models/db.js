6ce3cd662cf879c4615ac16ca75cd9fc8403bfe5
Gaz1676 <gazfleming@gmail.com>
2017-10-10 12:14:01 +0100

    Introduced mongoose function to load the data.json file

--- app/models/db.js
+++ app/models/db.js
@@ -4,9 +4,9 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 // changed to localhost:12345
-//let dbURI = 'mongodb://donationuser:donationuser@ds113925.mlab.com:13925/donation-web';
+let dbURI = 'mongodb://donationuser:donationuser@ds113925.mlab.com:13925/donation-web';
 
-let dbURI = 'mongodb://localhost:12345/donation';
+//let dbURI = 'mongodb://localhost:12345/donation';
 if (process.env.NODE_ENV === 'production') {
   dbURI = process.env.MONGOLAB_URI;
 }
@@ -23,6 +23,23 @@ mongoose.connection.on('error', function (err) {
 
 mongoose.connection.on('disconnected', function () {
   console.log('Mongoose disconnected');
+});
+
+mongoose.connection.on('connected', function () {
+  console.log('Mongoose connected to ' + dbURI);
+  if (process.env.NODE_ENV === 'production') {
+    var seeder = require('mongoose-seeder');
+    const data = require('./data.json');
+    const Donation = require('./donation');
+    const User = require('./user');
+    const Candidate = require('./candidate.js'); // loaded candidate import
+    seeder.seed(data, { dropDatabase: false, dropCollections: true }).then(dbData => {
+      console.log('preloading Test Data');
+      console.log(dbData);
+    }).catch(err => {
+      console.log(error);
+    });
+  }
 });
 
 // This will establish a connection to the database.
