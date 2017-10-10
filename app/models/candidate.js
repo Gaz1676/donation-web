f377fc28d9edb7cbe944e38b2945fb56981bb91e
Gaz1676 <gazfleming@gmail.com>
2017-10-10 12:20:02 +0100

    Introduced a new schema to represent a Candidate in our app:

---
+++ app/models/candidate.js
@@ -0,0 +1,13 @@
+'use strict';
+
+//  new schema to represent a Candidate in our app
+const mongoose = require('mongoose');
+
+const candidateSchema = mongoose.Schema({
+  firstName: String,
+  lastName: String,
+  office: String,
+});
+
+const Candidate = mongoose.model('Candidate', candidateSchema);
+module.exports = Candidate;
